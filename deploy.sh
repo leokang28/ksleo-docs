92c078a09e63c52a0a51fb549bef5290778df07b
leokang28 <leokang28@hotmail.com>
2019-11-12 16:01:03 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,9 @@ git commit -m 'update'
 git push
 
 cd ./docs/.vuepress/dist
+git init
 git add .
 git commit -m 'update'
-git push
+# git push
+
+git push -f git@github.com:leokang28/leokang28.github.io.git master
